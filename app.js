3bc5e69716c6b97b71462fcf9c14d3eb51789831
Derrick Strong <derrickmstrong@gmail.com>
2020-05-14 01:37:53 -0500

    Updated Covid-19 theme

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 // Define Players
 const players = [
   {
-    name: 'Put S😷fety First',
+    name: 'Put Y😷ur Safety First',
     emoji: '😷',
   },
   {
-    name: 'R🦠na Strike Again',
+    name: 'R🦠na Strikes Again',
     emoji: '🦠',
   },
 ];
@@ -34,46 +34,49 @@ const resetStyles = {
   boxShadow: 'inset 0px 1px 0px 0px #f29c93',
   background: 'linear-gradient(to bottom, #fe1a00 5%, #ce0100 100%)',
   backgroundColor: '#fe1a00',
-  borderRadius: '6px',
+  borderRadius: '10px',
   border: '1px solid #d83526',
-  display: 'inline-block',
   cursor: 'pointer',
   color: '#ffffff',
-  fontFamily: 'Arial',
-  fontSize: '15px',
+  fontFamily: 'monospace',
+  fontSize: '18px',
   fontWeight: 'bold',
   padding: '1rem',
-  textDecoration: 'none',
   textShadow: '0px 1px 0px #b23e35',
+  position: 'relative',
+  top: '-4rem'
 };
 
-const readyStyles = {
+const insertCoinsStyles = {
   fontSize: '2rem',
   fontWeight: 'bold',
   textAlign: 'center',
   position: 'relative',
   color: 'red',
-  width: '300px'
+  width: '300px',
+  paddingTop: '2rem'
 };
 
 const announceWinnerStyles = {
-  color: 'black',
-  fontSize: '1.15em',
-  padding: '.5em',
+  color: '#2B1061',
+  backgroundColor: 'rgba(189, 172, 247, 0.8)',
+  fontSize: '1.5em',
   textAlign: 'center',
+  position: 'relative',
+  top: '-11.5rem',
 };
 
-// Ready Statement
-function ready() {
-  const ready = document.createElement('div');
-  ready.textContent = 'INSERT 💰 COINS';
-  Object.assign(ready.style, readyStyles);
-  document.querySelector('.middle').appendChild(ready);
+// insertCoins Statement
+function insertCoins() {
+  const insertCoins = document.createElement('div');
+  insertCoins.textContent = '💰INSERT COIN💰';
+  Object.assign(insertCoins.style, insertCoinsStyles);
+  document.querySelector('#winner').appendChild(insertCoins);
   setTimeout(function () {
-    ready.style.display = 'none';
-  }, 2000);
+    insertCoins.style.display = 'none';
+  }, 4000);
 }
-ready()
+insertCoins()
 
 // Listen for clicks
 for (let i = 0; i < cells.length; i++) {
@@ -97,44 +100,46 @@ function cellClicked() {
   // Check if P1 is the winner
   for (let i = 0; i < combo.length; i++) {
     // Check if game is a DRAW
-    // FIXME: Figure out why the Draw check is not working properly; Instead of saying Draw Once and add one button its addding 8
-    if (
-      cells[0].textContent != '' &&
-      cells[1].textContent != '' &&
-      cells[2].textContent != '' &&
-      cells[3].textContent != '' &&
-      cells[4].textContent != '' &&
-      cells[5].textContent != '' &&
-      cells[6].textContent != '' &&
-      cells[7].textContent != '' &&
-      cells[8].textContent != ''
-    ) {
-      gameOver = true;
-      // Announce Winner
-      // const announceWinner = document.createElement('div');
-      // announceWinner.textContent = `DRAW!!!`;
-      // Object.assign(announceWinner.style, announceWinnerStyles);
-      // document.querySelector('#winner').appendChild(announceWinner);
-
-      // Reset Game Board
-      const btn = document.querySelector('.btn');
-      const resetBtn = document.createElement('button');
-      resetBtn.textContent = 'TRY AGAIN';
-
-      // Apply resetStyles
-      Object.assign(resetBtn.style, resetStyles);
-
-      // Attach resetBtn to document
-      btn.appendChild(resetBtn);
-
-      resetBtn.addEventListener('click', resetGame);
-
-      function resetGame() {
-        setTimeout(function () {
-          location = '';
-        }, 500);
-      }
-    } else if (
+     /*
+       FIXME: Figure out why the Draw check is not working properly; Instead of saying Draw Once and add one button its addding 8
+      if (
+        cells[0].textContent != '' &&
+        cells[1].textContent != '' &&
+        cells[2].textContent != '' &&
+        cells[3].textContent != '' &&
+        cells[4].textContent != '' &&
+        cells[5].textContent != '' &&
+        cells[6].textContent != '' &&
+        cells[7].textContent != '' &&
+        cells[8].textContent != ''
+      ) {
+        gameOver = true;
+        // Announce Winner
+        // const announceWinner = document.createElement('div');
+        // announceWinner.textContent = `DRAW!!!`;
+        // Object.assign(announceWinner.style, announceWinnerStyles);
+        // document.querySelector('#winner').appendChild(announceWinner);
+  
+        // Reset Game Board
+        const btn = document.querySelector('.btn');
+        const resetBtn = document.createElement('button');
+        resetBtn.textContent = 'TRY AGAIN';
+  
+        // Apply resetStyles
+        Object.assign(resetBtn.style, resetStyles);
+  
+        // Attach resetBtn to document
+        btn.appendChild(resetBtn);
+  
+        resetBtn.addEventListener('click', resetGame);
+  
+        function resetGame() {
+          setTimeout(function () {
+            location = '';
+          }, 500);
+        }
+      } else
+     */ if (
       cells[combo[i][0]].textContent == players[0].emoji &&
       cells[combo[i][1]].textContent == players[0].emoji &&
       cells[combo[i][2]].textContent == players[0].emoji
